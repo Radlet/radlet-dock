77f8a74a024d1f657950781125c00630930f791a
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-11-29 20:21:15 +0530

    basic route working, cmake updated, patch to crow added

--- libs/crow/inc/crow_all.h
+++ libs/crow/inc/crow_all.h
@@ -8,6 +8,12 @@
 #include <iostream>
 #include <boost/optional.hpp>
 
+#if BOOST_VERSION >= 107000
+#define GET_IO_SERVICE(s) ((boost::asio::io_context&)(s).get_executor().context())
+#else
+#define GET_IO_SERVICE(s) ((s).get_io_service())
+#endif
+
 namespace crow
 {
 // ----------------------------------------------------------------------------
@@ -3350,7 +3356,7 @@ namespace crow
 
         boost::asio::io_service& get_io_service()
         {
-            return socket_.get_io_service();
+            return GET_IO_SERVICE(socket_);
         }
 
         tcp::socket& raw_socket()
@@ -3425,7 +3431,7 @@ namespace crow
 
         boost::asio::io_service& get_io_service()
         {
-            return raw_socket().get_io_service();
+            return GET_IO_SERVICE(raw_socket());
         }
 
         template <typename F> 
