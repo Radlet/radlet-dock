9495a1cc107eba98eb54f09e97d7f0777c6b6108
dhirajfx3 <dhirajfx3@gmail.com>
2019-12-30 00:20:53 +0530

    Modified test script to run from options. A.k.a Aption diye the

--- run_build_tests.sh
+++ run_build_tests.sh
@@ -1,3 +1,19 @@
-docker build -t lattice_hub.test -f ./Docker/dev.Dockerfile .
-docker build -t lattice_hub.test -f ./Docker/deploy.Dockerfile .
-docker build -t lattice_hub.test -f ./Docker/env.Dockerfile .
+#!/bin/bash
+if [[ $# -eq 0 || $1 == "-help" ]]; then
+  echo "Usage ./run_build_tests.sh -<option>"
+  echo "      <option>: b - build"
+  echo "               d - deploy"
+  echo "               t - unittest"
+  echo "               a - all"
+elif [[ "$1" == "-b" || $1 == "-build" ]]; then
+  docker build -t lattice_hub.test -f ./Docker/dev.Dockerfile .
+elif [[ "$1" == "-d" || $1 == "-deploy" ]]; then
+  docker build -t lattice_hub.test -f ./Docker/deploy.Dockerfile .
+elif [[ "$1" == "-t" || $1 == "-unittest" ]]; then
+  echo "Unit tests to be added soon."
+elif [[ "$1" == "-a" || $1 == "-all" ]]; then
+  docker build -t lattice_hub.test -f ./Docker/dev.Dockerfile .
+  docker build -t lattice_hub.test -f ./Docker/deploy.Dockerfile .
+else
+  echo "Invalid command. Try ./run_build_test.sh -help"
+fi
