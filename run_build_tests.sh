7f41adc137fe3c56c84fcb07ddcbc928baebbffb
dhirajfx3 <dhirajfx3@gmail.com>
2019-12-29 23:54:02 +0530

    Added script to run build tests.

---
+++ run_build_tests.sh
@@ -0,0 +1,3 @@
+docker build -t lattice_hub.test -f ./Docker/dev.Dockerfile .
+docker build -t lattice_hub.test -f ./Docker/deploy.Dockerfile .
+docker build -t lattice_hub.test -f ./Docker/env.Dockerfile .
