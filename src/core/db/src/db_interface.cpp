14f1d1cb01c9fb6673c66486226c0d34048d4308
humble_D <aakashs.mallik.cse15@itbhu.ac.in>
2020-01-20 21:59:12 +0530

    device db basic interface working (#45)

--- src/core/db/src/db_interface.cpp
+++ src/core/db/src/db_interface.cpp
@@ -1,1 +1,25 @@
-// db interface src
+// std
+#include <iostream>
+
+// implementation file
+#include "db_interface.h"
+
+sqlite3pp::database *database::DeviceDbInterface::db = nullptr;
+
+bool database::DeviceDbInterface::connect(std::string db_file_path) {
+  db = new sqlite3pp::database(db_file_path.c_str());
+
+  std::cout << "db file created" << std::endl;
+}
+
+bool database::DeviceDbInterface::disconnect() { delete db; }
+
+bool database::DeviceDbInterface::addDevice(std::string mac, std::string ip,
+                                            std::string port) {
+  sqlite3pp::command cmd(
+      *db, "INSERT INTO devices (mac, ip, port) VALUES (?, ?, ?)");
+  cmd.binder() << mac << ip << port;
+  cmd.execute();
+
+  std::cout << "device added successfully" << std::endl;
+}
