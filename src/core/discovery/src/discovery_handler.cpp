362d1ae7e42eea6388bbf1f4e118e9852c7c950b
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-01-20 23:30:10 +0530

    UDP interface JSON parser added

--- src/core/discovery/src/discovery_handler.cpp
+++ src/core/discovery/src/discovery_handler.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <vector>
 
-// lib
-#include <boost/algorithm/string.hpp> 
+// 3rd party lib
+// #include <boost/algorithm/string.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 // internal modules
 #include "http_interface.h"
@@ -11,16 +13,16 @@
 // implementation file
 #include "discovery_handler.h"
 
+core::discovery::DiscoveryHandler::DiscoveryHandler() {}
 
-core::discovery::DiscoveryHandler::DiscoveryHandler(){
+void core::discovery::DiscoveryHandler::extract_message(
+    std::string raw_message) {
+  std::stringstream ss;
+  ss << raw_message;
+  boost::property_tree::ptree pt;
+  boost::property_tree::read_json(ss, pt);
 
-}
-
-void core::discovery::DiscoveryHandler::extract_message( std::string raw_message ){
-    std::vector<std::string> data_array; 
-    boost::split(data_array, raw_message, boost::is_any_of(" "));
-    std::cout << "mac: " << data_array[0] << std::endl;
-    std::cout << "ip: " << data_array[1] << std::endl;
-    std::cout << "port: " << data_array[2] << std::endl;
-    io::adaptor::tcp_interface::HttpSender::requestAttach(data_array[0], data_array[1], data_array[2]);
+  io::adaptor::tcp_interface::HttpSender::requestAttach(
+      pt.get<std::string>("ip"), pt.get<std::string>("port"),
+      pt.get<std::string>("mac"));
 }
