c846c7cf29374f483bc8fc1894dec617b94ffe3e
humble_D <aakashs.mallik.cse15@itbhu.ac.in>
2020-03-03 20:42:59 +0530

    Docker images for ARM and github actions support for ARM added (#83)
    * Docker files updated to build ARM images
    
    * Github actions build for ARM added

---
+++ Docker/x64/dev.Dockerfile
@@ -0,0 +1,20 @@
+# our base image
+FROM radlet/radlet_dock.env:x64
+
+# # copy files to container
+COPY . /usr/src/app
+
+# # compile source
+RUN mkdir -p /usr/src/app/build
+WORKDIR /usr/src/app/build
+RUN rm -rf *
+RUN cmake ..
+RUN make
+
+# expose port 
+# EXPOSE 8080
+# EXPOSE 30001/udp
+
+# If build fails, issue this command
+# docker rmi $(docker images -f “dangling=true” -q)
+# docker system prune
--- package.sh
+++ package.sh
@@ -1,38 +1,70 @@
 #!/bin/bash
 if [[ $# -eq 0 || $1 == "--help" ]]; then
-  echo "Usage ./run_build_tests.sh -<option>"
+  echo "Usage ./run_build_tests.sh -<option> --[architecture]"
   echo "      <option>: b - build"
-  echo "               d - deploy"
-  echo "               ud - update deployment image"
-  echo "               mt - manual testing"
-  echo "               e - create environment image"
-  echo "               d - deploy"
-  echo "               t - unittest"
-  echo "               a - all"
-elif [[ "$1" == "-b" || $1 == "--build" ]]; then
-  docker build -t radlet_dock.dev -f ./Docker/dev.Dockerfile .
+  echo "                d - deploy"
+  echo "                ud - update deployment image"
+  echo "                mt - manual testing"
+  echo "                e - create environment image"
+  echo "                d - deploy"
+  echo "                t - unittest"
+  echo "                a - all"
+  echo "                      "
+  echo "      <architecture>: arm - ARM 32bit"
+elif [ "$2" == "--arm" ]; then
+  if [[ "$1" == "-b" || $1 == "--build" ]]; then
+    docker build -t radlet_dock.dev:arm -f ./Docker/arm/dev.Dockerfile .
 
-elif [[ "$1" == "-ud" || $1 == "--update-deploy" ]]; then
-  docker build -t radlet/radlet_dock -f ./Docker/deploy.Dockerfile .
-  docker push radlet/radlet_dock
+  elif [[ "$1" == "-ud" || $1 == "--update-deploy" ]]; then
+    docker build -t radlet/radlet_dock:arm -f ./Docker/arm/deploy.Dockerfile .
+    docker push radlet/radlet_dock:arm
 
-elif [[ "$1" == "-d" || $1 == "--deploy" ]]; then
-  mkdir -p hub_sensor_data
-  docker-compose -f ./Docker/deploy.docker-compose.yml up
+  elif [[ "$1" == "-d" || $1 == "--deploy" ]]; then
+    mkdir -p hub_sensor_data
+    docker-compose -f ./Docker/arm/deploy.docker-compose.yml up
 
-elif [[ "$1" == "-t" || $1 == "--unit-test" ]]; then
-  echo "Unit tests to be added soon."
+  elif [[ "$1" == "-t" || $1 == "--unit-test" ]]; then
+    echo "Unit tests to be added soon."
 
-elif [[ "$1" == "-mt" || $1 == "--manual-test" ]]; then
-  mkdir -p hub_sensor_data
-  docker-compose -f ./Docker/dev.docker-compose.yml run radlet_dock
+  elif [[ "$1" == "-mt" || $1 == "--manual-test" ]]; then
+    mkdir -p hub_sensor_data
+    docker-compose -f ./Docker/arm/dev.docker-compose.yml run radlet_dock
 
-elif [[ "$1" == "-e" || $1 == "--env" ]]; then
-  docker build -t radlet/radlet_dock.env -f ./Docker/env.Dockerfile .
+  elif [[ "$1" == "-e" || $1 == "--env" ]]; then
+    docker build -t radlet/radlet_dock.env:arm -f ./Docker/arm/env.Dockerfile .
 
-elif [[ "$1" == "-a" || $1 == "-all" ]]; then
-  docker build -t radlet_dock.test -f ./Docker/dev.Dockerfile .
+  elif [[ "$1" == "-a" || $1 == "-all" ]]; then
+    docker build -t radlet_dock.test -f ./Docker/arm/dev.Dockerfile .
 
+  else
+    echo "Invalid command. Try ./run_build_test.sh --help"
+  fi
 else
-  echo "Invalid command. Try ./run_build_test.sh --help"
+  if [[ "$1" == "-b" || $1 == "--build" ]]; then
+    docker build -t radlet_dock.dev:x64 -f ./Docker/x64/dev.Dockerfile .
+
+  elif [[ "$1" == "-ud" || $1 == "--update-deploy" ]]; then
+    docker build -t radlet/radlet_dock:x64 -f ./Docker/x64/deploy.Dockerfile .
+    docker push radlet/radlet_dock:x64
+
+  elif [[ "$1" == "-d" || $1 == "--deploy" ]]; then
+    mkdir -p hub_sensor_data
+    docker-compose -f ./Docker/x64/deploy.docker-compose.yml up
+
+  elif [[ "$1" == "-t" || $1 == "--unit-test" ]]; then
+    echo "Unit tests to be added soon."
+
+  elif [[ "$1" == "-mt" || $1 == "--manual-test" ]]; then
+    mkdir -p hub_sensor_data
+    docker-compose -f ./Docker/x64/dev.docker-compose.yml run radlet_dock
+
+  elif [[ "$1" == "-e" || $1 == "--env" ]]; then
+    docker build -t radlet/radlet_dock.env:x64 -f ./Docker/x64/env.Dockerfile .
+
+  elif [[ "$1" == "-a" || $1 == "-all" ]]; then
+    docker build -t radlet_dock.test -f ./Docker/x64/dev.Dockerfile .
+
+  else
+    echo "Invalid command. Try ./run_build_test.sh --help"
+  fi
 fi
